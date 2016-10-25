15b9a736f8d00fa7a4733463e5ee30faf523911d
TheToxicKiller <kalo.poljakov@gmail.com>
2016-10-25 11:06:25 +0300

    Create 01_domasho.c

---
+++ G/03/14/01_domasho.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,a,sum=0,i=0;
+float avr;
+scanf("%d",&n);
+while(i<n)
+{
+scanf("%d",&a);
+sum=sum+a;
+i++;
+}
+avr=(float)sum/n;
+printf("\nSum: %d",sum);
+printf("\nAvg: %.2f",avr);
+return 0;
+}
